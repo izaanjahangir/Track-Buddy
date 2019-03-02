58b014eb86a84bd8ce1c2e5e869015cb061ecc88
izaanjahangir <izaanjahangir2@gmail.com>
2019-03-03 00:01:54 +0500

    code cleanup

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import AppContainer from './src/router/router';
 export default class App extends React.Component {
   render() {
+    console.disableYellowBox = true;
+    
     return (
       <AppContainer />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
